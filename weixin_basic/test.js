5ab4e342125ecba1121a84f7ecc5602b00732913
WangAnqi <waqthss2012@163.com>
2014-11-26 00:31:18 +0800

    将custom_menu中的菜单json移到settings中，增添test.js用于测试，后期会删除

---
+++ weixin_basic/test.js
@@ -0,0 +1,16 @@
+var a = require("./settings.js")
+var cm = require("./custom_menu.js")
+/*a.get_custom_menu_with_book_acts([{
+                    "type": "click",
+                    "name": "新年音乐会",
+                    "key": 'get',
+                    "sub_button": []
+                },
+                {
+                    "type": "click",
+                    "name": "主题团日",
+                    "key": 'bind',
+                    "sub_button": []
+                }
+                ])*/
+cm.createMenu;
