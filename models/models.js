54db9d48e967842f59f37f4c4ef29a545da55721
Levy <zly.george@163.com>
2014-12-06 13:15:36 +0800

    更新了模型，增加座位数据库

--- models/models.js
+++ models/models.js
@@ -3,14 +3,16 @@ var tickets = "ticket";
 var activities = "activity";
 var students = "student";
 var admins = "manager";
+var seats = "seat"
 
 
 exports.tickets = tickets;
 exports.activities = activities;
 exports.students = students;
 exports.admins = admins;
+exports.seats = seats;
 
-exports.db = mongojs('mongodb://localhost/test23', [tickets, activities, students, admins]);
+exports.db = mongojs('mongodb://localhost/test23', [tickets, activities, students, admins, seats]);
 
 exports.getIDClass=function(idValue)
 {
