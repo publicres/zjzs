ba80b504b3887657f44def9a480068db53bf7b46
zhongyx12 <zhongyx4869@163.com>
2014-12-09 18:28:04 +0800

    前后合体！

---
+++ weixin_basic/test.js
@@ -0,0 +1,25 @@
+var set = require("./settings.js")
+var cm = require("./custom_menu.js")
+var at = require('./access_token')
+
+//at.getAccessToken(cm.createMenu)
+//at.getAccessToken(cm.getMenu)
+//at.getAccessToken(cm.deleteMenu)
+
+/*
+btn = [{
+            "type": "click",
+            "name": "新年晚会",
+            "key": 'ticket_book_what',
+             "sub_button": []
+        },
+        {
+            "type": "click",
+            "name": "主题团日",
+            "key": 'ticket_get',
+            "sub_button": []
+        }]
+cm.modifyMenu(btn);
+at.getAccessToken(cm.createMenu)
+*/
+
