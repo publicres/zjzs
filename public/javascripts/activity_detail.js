51883141a188cc97ac10ae188e9062e805a0f0d1
BaoYu <fairytale12345@163.com>
2014-12-14 13:29:51 +0800

    提示显示修改

--- public/javascripts/activity_detail.js
+++ public/javascripts/activity_detail.js
@@ -523,6 +523,14 @@ function beforeSubmit(formData, jqForm, options) {
         }
     }
     if (lackArray.length > 0) {
+        var d = $('#resultHolder');
+        if (d.hasClass("resultError")) {
+            d.removeClass("resultError");
+        }
+        if (d.hasClass("resultSuccess")) {
+            d.removeClass("resultSuccess");
+        }
+        d.addClass("resultError");
         setResult('以下字段是必须的，请补充完整后再提交：\r\n' + lackArray.join('、'));
         $('#continueBtn').click(function() {
             showForm();
@@ -580,7 +588,31 @@ function submitResponse(data) {
 }
 
 function submitError(xhr) {
-    setResult(xhr.responseText || '<null>');
+    var d = $('#resultHolder');
+    if (d.hasClass("resultError")) {
+        d.removeClass("resultError");
+    }
+    if (d.hasClass("resultSuccess")) {
+        d.removeClass("resultSuccess");
+    }
+    var str = xhr.responseText;
+    if (!str) {
+        str = "<null>";
+        d.addClass("resultError");
+    }
+    else {
+        var arr = str.split("#");
+        if (arr[0] == "200") {
+            d.addClass("resultSuccess");
+            str = arr[1];
+        }
+        else {
+            d.addClass("resultError");
+            str = arr[1];
+        }
+    }
+    //setResult(xhr.responseText || '<null>');
+    setResult(str);
     $('#continueBtn').click(function() {
         showForm();
     });
