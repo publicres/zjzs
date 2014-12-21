0e7df47f557370cb9cf72027663168cde10f8153
Levy <zly.george@163.com>
2014-12-21 17:47:15 +0800

    改进了我的票夹显示方式

--- weixin_handler/handler_ticket.js
+++ weixin_handler/handler_ticket.js
@@ -412,6 +412,14 @@ exports.faire_list_ticket=function(msg,res)
                 {
                     actMap[docs1[i]._id]=docs1[i];
                 }
+
+                var tmpEle;
+                tmpEle={};
+                tmpEle[template.rich_attr.title]="我的票夹";
+                tmpEle[template.rich_attr.description]=
+                    "以下列表中是您抢到的票。(如果超过9个则可能有省略)";
+
+                list2Render.push(tmpEle);
                 for (var i=0;i<docs.length;i++)
                 {
                     list2Render.push(renderTicketList(docs[i],actMap[docs[i].activity],false));
