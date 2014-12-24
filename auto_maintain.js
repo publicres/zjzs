103550e12566e0b9d74ce32f59108cdc5c25c4aa
Levy <zly.george@163.com>
2014-12-24 10:24:23 +0800

    在autoRefresh控制整个自动更新菜单的功能

--- auto_maintain.js
+++ auto_maintain.js
@@ -2,6 +2,7 @@ var sweeper = require("./models/sweeper");
 var assigner= require("./models/assign_seat");
 var cm = require("./weixin_basic/custom_menu");
 var act_info = require('./weixin_basic/activity_info');
+var urls = require('./address_configure.js');
 
 var currentHour=(new Date()).getHours();
 if (currentHour!=4)
@@ -13,7 +14,10 @@ sweeper(function()
 {
     assigner(function()
     {
-        cm.isExit=true;
-        act_info.getCurrentActivity(cm.autoClearOldMenus);
+        if (urls.autoRefresh==true)
+        {
+            cm.isExit=true;
+            act_info.getCurrentActivity(cm.autoClearOldMenus);
+        }    
     });
 });
