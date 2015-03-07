5c811e649c8bb2ed87751b9f77a3b7f7b307f629
Levy <zly.george@163.com>
2015-03-07 09:19:10 +0800

    更改定时更新菜单的频率

--- auto_maintain.js
+++ auto_maintain.js
@@ -8,20 +8,32 @@ var currentHour=(new Date()).getHours();
 if (currentHour!=4)
 {
     console.log("Wrong time to exec.")
-    process.exit(0);
-}
-sweeper(function()
-{
-    assigner(function()
+    if (urls.autoRefresh==true)
     {
-        if (urls.autoRefresh==true)
+        cm.isExit=true;
+        act_info.getCurrentActivity(cm.autoClearOldMenus);
+    }
+    else
+    {
+        process.exit(0);
+    }
+}
+else
+{
+    sweeper(function()
+    {
+        assigner(function()
         {
-            cm.isExit=true;
-            act_info.getCurrentActivity(cm.autoClearOldMenus);
-        }
-        else
-        {
-            process.exit(0);
-        }
+            if (urls.autoRefresh==true)
+            {
+                cm.isExit=true;
+                act_info.getCurrentActivity(cm.autoClearOldMenus);
+            }
+            else
+            {
+                process.exit(0);
+            }
+        });
     });
-});
+}
+
